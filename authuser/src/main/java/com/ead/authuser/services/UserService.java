97d0fb6e51556f16894f913c84128676727c451e
Juan Lira <juancassiano@hotmail.com>
2025-07-13 22:43:07 -0300

    feat: add pageable in method getAllUsers

--- authuser/src/main/java/com/ead/authuser/services/UserService.java
+++ authuser/src/main/java/com/ead/authuser/services/UserService.java
@@ -4,6 +4,9 @@ import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
 import com.ead.authuser.models.UserModel;
 
 public interface UserService {
@@ -19,6 +22,8 @@ public interface UserService {
   boolean existsByUsername(String username);
 
   boolean existsByEmail(String email);
+
+  Page<UserModel> findAll(Pageable pageable);
 
   
 }
