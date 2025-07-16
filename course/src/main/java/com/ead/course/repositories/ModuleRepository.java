b846ab9aa41afd6ca9c29478b99e55ac188a8796
Juan Lira <juancassiano@hotmail.com>
2025-07-15 23:26:30 -0300

    feat: add fk in tables

--- course/src/main/java/com/ead/course/repositories/ModuleRepository.java
+++ course/src/main/java/com/ead/course/repositories/ModuleRepository.java
@@ -2,12 +2,13 @@ package com.ead.course.repositories;
 
 import java.util.UUID;
 
-import org.aspectj.apache.bcel.classfile.ModuleMainClass;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import com.ead.course.models.ModuleModel;
+
 
 @Repository
-public interface ModuleRepository extends JpaRepository<ModuleMainClass, UUID> {
+public interface ModuleRepository extends JpaRepository<ModuleModel, UUID> {
   
 }
