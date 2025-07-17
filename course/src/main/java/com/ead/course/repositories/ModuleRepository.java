194560e31a4196a06a55975b19b36c56f0da6418
Juan Lira <juancassiano@hotmail.com>
2025-07-16 22:14:47 -0300

    feat: add lazy fetch and nativeQuery select

--- course/src/main/java/com/ead/course/repositories/ModuleRepository.java
+++ course/src/main/java/com/ead/course/repositories/ModuleRepository.java
@@ -1,8 +1,11 @@
 package com.ead.course.repositories;
 
+import java.util.List;
 import java.util.UUID;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import com.ead.course.models.ModuleModel;
@@ -11,4 +14,6 @@ import com.ead.course.models.ModuleModel;
 @Repository
 public interface ModuleRepository extends JpaRepository<ModuleModel, UUID> {
   
+  @Query(value = "SELECT * from TB_MODULES where course_couse_id =:courseId", nativeQuery = true)
+  List<ModuleModel> findAllModulesIntoCourse(@Param("courseId") UUID courseId);
 }
