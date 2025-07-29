13ce73b457fa1a189efcebbbf35ad7077de8cbd6
Juan Lira <juancassiano@hotmail.com>
2025-07-28 23:32:01 -0300

    feat: add pagination and specification module

--- course/src/main/java/com/ead/course/repositories/ModuleRepository.java
+++ course/src/main/java/com/ead/course/repositories/ModuleRepository.java
@@ -5,6 +5,7 @@ import java.util.Optional;
 import java.util.UUID;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
@@ -13,7 +14,7 @@ import com.ead.course.models.ModuleModel;
 
 
 @Repository
-public interface ModuleRepository extends JpaRepository<ModuleModel, UUID> {
+public interface ModuleRepository extends JpaRepository<ModuleModel, UUID>, JpaSpecificationExecutor<ModuleModel> {
   
   @Query(value = "SELECT * from TB_MODULES where course_course_id =:courseId", nativeQuery = true)
   List<ModuleModel> findAllModulesIntoCourse(@Param("courseId") UUID courseId);
