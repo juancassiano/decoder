90add7cd4abd288cb2f67ee6dda9a4ceedc1c044
Juan Lira <juancassiano@hotmail.com>
2025-07-28 22:31:18 -0300

    feat: add pagination and specification course

--- course/src/main/java/com/ead/course/repositories/CourseRepository.java
+++ course/src/main/java/com/ead/course/repositories/CourseRepository.java
@@ -3,11 +3,12 @@ package com.ead.course.repositories;
 import java.util.UUID;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.stereotype.Repository;
 
 import com.ead.course.models.CourseModel;
 
 @Repository
-public interface CourseRepository extends JpaRepository<CourseModel, UUID> {
-  
+public interface CourseRepository extends JpaRepository<CourseModel, UUID>, JpaSpecificationExecutor<CourseModel> {
+
 }
--- course/src/main/java/com/ead/course/services/CourseService.java
+++ course/src/main/java/com/ead/course/services/CourseService.java
@@ -1,8 +1,11 @@
 package com.ead.course.services;
 
-import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.domain.Specification;
 
 import com.ead.course.models.CourseModel;
 
@@ -13,5 +16,5 @@ public interface CourseService {
 
   Optional<CourseModel> findById(UUID courseId);
 
-  List<CourseModel> findAll();
+  Page<CourseModel> findAll(Specification<CourseModel> spec, Pageable pageable);
 }
