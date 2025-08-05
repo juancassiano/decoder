76d5c12ebe75983169c0a4f45041b332603d4ff8
Juan Lira <juancassiano@hotmail.com>
2025-08-04 21:08:41 -0300

    feat: implement log with logback

--- authuser/src/main/java/com/ead/authuser/controllers/AuthenticationController.java
+++ authuser/src/main/java/com/ead/authuser/controllers/AuthenticationController.java
@@ -3,12 +3,15 @@ package com.ead.authuser.controllers;
 import java.time.LocalDateTime;
 import java.time.ZoneId;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -25,6 +28,8 @@ import com.fasterxml.jackson.annotation.JsonView;
 @CrossOrigin(origins = "*", maxAge = 3600)
 @RequestMapping("/auth")
 public class AuthenticationController {
+
+  Logger logger = LoggerFactory.getLogger(AuthenticationController.class);
 
   @Autowired
   private UserService userService;
@@ -47,5 +52,15 @@ public class AuthenticationController {
     userService.save(userModel);
 
     return ResponseEntity.status(HttpStatus.CREATED).body(userModel);
+  }
+
+  @GetMapping()
+  public String index(){
+    logger.trace("TRACE");
+    logger.debug("DEBUG");
+    logger.info("INFO");
+    logger.warn("WARN");
+    logger.error("ERROR");
+    return "Logging Spring Boot";
   }
 }
